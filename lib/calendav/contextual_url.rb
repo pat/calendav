eb6045bd609b0d840b3e1f6a1c97f0a9970c9dea
FaridAP <fanooshehpour@petalmd.com>
2023-02-10 09:14:43 -0500

    encode permitted-character in the url

--- lib/calendav/contextual_url.rb
+++ lib/calendav/contextual_url.rb
@@ -4,15 +4,28 @@ require "uri"
 
 module Calendav
   class ContextualURL
+    PERMITTED_SPECIAL_CHARACTER = %w[! # $ & ' * + - = ? ^ _ ` { | } ~"].freeze
+
     def self.call(host, url_or_path)
       host = URI(host)
       return host.to_s if url_or_path.nil? || url_or_path.empty?
 
       if url_or_path.start_with?("/")
+        url_or_path = encode_path(url_or_path)
+
         host.dup.tap { |new_url| new_url.path = url_or_path }.to_s
       else
         URI(url_or_path).to_s
       end
+    end
+
+    def self.encode_path(url)
+      url.split("/").map do |path|
+        PERMITTED_SPECIAL_CHARACTER.each do |ch|
+          path.gsub!(ch, CGI.escape(ch))
+        end
+        path
+      end.join("/") + "/"
     end
   end
 end
